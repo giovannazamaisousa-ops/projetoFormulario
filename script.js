67f229567605ad37444c79d4310a679fca87ce5d
giovannazs <giovanna.zamai.sousa@escola.pr.gov.br>
2026-04-30 11:40:28 -0300

    a

--- script.js
+++ script.js
@@ -58,5 +58,7 @@ window.onload = function () {
 function excluirCarro(id) {
     let carros = JSON.parse(localStorage.getItem('carros')) || [];
 
-carros = carros.filter(carro => carro.id);
+    carros = carros.filter(carro => carro.id !== id);
+
+    localStorage.setItem('carros', JSON.stringify(carros));
 }
